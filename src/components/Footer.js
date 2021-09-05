ab555bb7689faf63116d6cf577671fee29e5a501
Rand92 <s.rund@yahoo.com>
2021-09-06 02:08:11 +0300

    lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <>
+                <p> Rand Albqour </p>
+            </>
+        )
+    }
+}
+
+export default Footer
