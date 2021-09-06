a99560915cd1d616a34e5f8fb870c26488d1a85c
Rand92 <s.rund@yahoo.com>
2021-09-06 23:05:53 +0300

    lab02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import Main from './Main';
 
  class HornedBeast extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            likes: 0,
+        }
+    }
+    incrementLikes =()=>{
+        this.setState({
+            likes:this.state.likes+1,
+        });
+    }
     render() {
         return (
             <>
               <h2> {this.props.title}</h2>  
-            <img src={this.props.imageSrc} alt=""/> 
+            <img src={this.props.imageSrc} alt="" onClick={this.incrementLikes} /> 
             <p> {this.props.description}</p>
+            <h3> likes: {this.state.likes} ❤️</h3>
             
             </>
         )
